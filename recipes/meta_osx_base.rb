406f813cf8b35cd19e0310419fdb726d5b40a51b
Ludovic Claude <ludovic.claude54@googlemail.com>
2013-04-08 17:36:20 +0200

    Add tig, Dropbox and Evernote

--- recipes/meta_osx_base.rb
+++ recipes/meta_osx_base.rb
@@ -12,6 +12,8 @@ include_recipe "pivotal_workstation::defaults_fast_key_repeat_rate"
 include_recipe "pivotal_workstation::menumeters"
 include_recipe "pivotal_workstation::bettertouchtool"
 include_recipe "pivotal_workstation::1password"
+include_recipe "pivotal_workstation::dropbox"
+include_recipe "pivotal_workstation::evernote"
 
 # include_recipe "pivotal_workstation::sshd_on"
-# include_recipe "pivotal_workstation::dropbox"
+
